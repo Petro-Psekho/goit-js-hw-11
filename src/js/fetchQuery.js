618369cc8064a956c7111533dfcee27cf57967d6
Petro Psekho <psekho.pp@gmail.com>
2022-12-28 17:01:21 +0200

    add searchParams in fetchQuery

--- src/js/fetchQuery.js
+++ src/js/fetchQuery.js
@@ -2,7 +2,7 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = 'key=32395796-06911cceb0b80396a7d5298f8';
 const imageType = 'image_type=photo';
 const orientation = 'orientation=horizontal';
-const safeSearch = 'safesearch=true';
+// const safeSearch = 'safesearch=true';
 
 export default async function fetchQuery(searchQuery) {
   const options = {
@@ -10,8 +10,14 @@ export default async function fetchQuery(searchQuery) {
   };
 
   try {
+    const searchParams = new URLSearchParams({
+      page: 5,
+      per_page: 12,
+      safesearch: true,
+    });
+
     const response = await fetch(
-      `${BASE_URL}?${API_KEY}&q=${searchQuery}&${imageType}&${orientation}&${safeSearch}&page=2&per_page=5`,
+      `${BASE_URL}?${API_KEY}&q=${searchQuery}&${imageType}&${orientation}&${searchParams}`,
       options
     );
     return await response.json();
