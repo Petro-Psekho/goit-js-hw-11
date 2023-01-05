7da44bd9c52dfdcabd76661112a31d9fb48e6cdf
Petro Psekho <psekho.pp@gmail.com>
2023-01-05 21:38:13 +0200

    add styles

--- src/js/smoothPageScrolling.js
+++ src/js/smoothPageScrolling.js
@@ -3,8 +3,6 @@ import { refs } from './refs';
 export function smoothScroll() {
   const { height } = refs.gallery.firstElementChild.getBoundingClientRect();
 
-  console.log({ height });
-
   window.scrollBy({
     top: height * 2,
     behavior: 'smooth',
